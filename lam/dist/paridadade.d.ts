acd2acb56844efb0fb12725cac0783e11f79c7b5
MiketheEmily45 <miguel@psyber.com.br>
2026-03-20 11:04:02 -0300

    teste

--- lam/dist/paridadade.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=paridadade.d.ts.map
--- lam/src/pesquisaremails.ts
+++
@@ -1,9 +0,0 @@
-function pesquisar (email: string, lista: string[]): boolean {
-    for (let i = 0; i < lista.length; i++) {
-        if (lista[i] === email) {
-            return true
-        }
-    }
-    return false
-}
-console.log(pesquisar("joao@example.com", ["maria@example.com", "joao@example.com", "pedro@example.com"]))
